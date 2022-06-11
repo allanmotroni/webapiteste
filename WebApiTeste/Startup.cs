b9088aeb293bdc60edbe73c414a10cf34f837c3a
Allan Motroni <allan.motroni@hotmail.com>
2022-06-11 10:50:04 -0300

    habilitando o swagger

--- WebApiTeste/Startup.cs
+++ WebApiTeste/Startup.cs
@@ -13,62 +13,63 @@ using WebApiTeste.Infrastructure.Repositories;
 
 namespace WebApiTeste
 {
-   public class Startup
-   {
-      public Startup(IConfiguration configuration)
-      {
-         Configuration = configuration;
-      }
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-      public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-      // This method gets called by the runtime. Use this method to add services to the container.
-      public void ConfigureServices(IServiceCollection services)
-      {
-         services.AddControllers();
-         services.AddSwaggerGen(c =>
-         {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApiTeste", Version = "v1" });
-         });
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApiTeste", Version = "v1" });
+            });
 
-         services.AddDbContext<DatabaseContext>(opt =>
-         {
-            var server = Configuration.GetConnectionString("DATABASE_SERVER");
-            var database = Configuration.GetConnectionString("DATABASE_NAME");
-            var login = Configuration.GetConnectionString("DATABASE_LOGIN");
-            var password = Configuration.GetConnectionString("DATABASE_PASSWORD");
+            services.AddDbContext<DatabaseContext>(opt =>
+            {
+                var server = Configuration.GetConnectionString("DATABASE_SERVER");
+                var database = Configuration.GetConnectionString("DATABASE_NAME");
+                var login = Configuration.GetConnectionString("DATABASE_LOGIN");
+                var password = Configuration.GetConnectionString("DATABASE_PASSWORD");
 
-            var connectionString = $"Server={server};Database={database};User Id={login};Password={password};MultipleActiveResultSets=True;";
-            opt.UseSqlServer(connectionString);
-         });
-         
-         services.AddHealthChecks();
+                var connectionString = $"Server={server};Database={database};User Id={login};Password={password};MultipleActiveResultSets=True;";
+                opt.UseSqlServer(connectionString);
+            });
 
-         services.AddScoped<ITesteService, TesteService>();
-         services.AddScoped<ITesteRepository, TesteRepository>();
-      }
+            services.AddHealthChecks();
 
-      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-      public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-      {
-         if (env.IsDevelopment())
-         {
-            app.UseDeveloperExceptionPage();
+            services.AddScoped<ITesteService, TesteService>();
+            services.AddScoped<ITesteRepository, TesteRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                //app.UseHttpsRedirection();
+            }
+
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApiTeste v1"));
-            //app.UseHttpsRedirection();
-         }
 
-         app.UseHealthChecks("/healthcheck");
+            app.UseHealthChecks("/healthcheck");
 
-         app.UseRouting();
+            app.UseRouting();
 
-         app.UseAuthorization();
+            app.UseAuthorization();
 
-         app.UseEndpoints(endpoints =>
-         {
-            endpoints.MapControllers();
-         });
-      }
-   }
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
 }
